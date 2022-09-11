8fba67479f9b726f51ef702f7834c654af9b8664
serghip <piedrabuena.sergio.f@gmail.com>
2022-09-11 18:42:47 -0300

    corrigo css

--- public/index.js
+++ public/index.js
@@ -1,9 +1,1 @@
-$(document).ready(function(){
-    //utilizar el heigth de pantalla del usuario para definir el height
-    var height = $(window).height();
-        //calculos y distribucion del height en base a la pantalla del usuario
-        $('#cajaPortada').height(height);
-        $('#cajaTextoPortada').height(height-(height/3));
-        $('#fondoTextoPortada').height(height/3);
-        $('#titulo').css('margin-top',height-(height/3));    
-});
+
